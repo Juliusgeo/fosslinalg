9ff67dbf1976b99b3545359f383b702157b68b1b
Juliusgeo <juliusgeo@gmail.com>
2016-05-20 23:02:40 -0400

    changed some stuff

--- rref.py
+++ rref.py
@@ -1,5 +1,5 @@
 import numpy as np
-m=np.matrix([[1,2,2],[3,4,4],[2,2,2]])
+m=np.matrix([[1,1,1,1],[1,1,1,1],[0,1,2,3],[0,1,2,3]])
 def swap(m, row1, row2):
     c=np.copy(m[0:,0:])
     c[row2]=m[row1]
@@ -9,12 +9,13 @@ def swap(m, row1, row2):
 def rref(m):
     i=0
     j=i
-    while i<=len(m) & j<=len(m[0]):
+    while i<=len(m)-1 and j<=len(m[0]):
+        print(i)
+        print(j)
         if m.item(i,j) == 0:
             for n in range(i,len(m)):
                 if swap(m,i,i+n).item(i,j) !=0:
                     m=np.copy(swap(m,i,i+n))
-
         m[i]=np.copy(m[i]/m.item(i,j))
     
         for k in range(i,len(m[0])):
