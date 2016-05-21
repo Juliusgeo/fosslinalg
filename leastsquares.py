ed0f31e61ddda9294b1759d487bf54304aef6cc8
Juliusgeo <juliusgeo@gmail.com>
2016-05-21 00:48:26 -0400

    removed test matrices
    now you have to define them yourself

--- leastsquares.py
+++ leastsquares.py
@@ -1,6 +1,6 @@
 import numpy as np
-a=np.matrix([[-1,2],[2,-3],[-1,3]])
-b=np.matrix([[4],[1],[2]])
+#a=np.matrix([[-1,2],[2,-3],[-1,3]])
+#b=np.matrix([[4],[1],[2]])
 def leastsquares(a,b):
     x=((a.transpose()*a).getI())*(a.transpose()*b)
     answer=""
